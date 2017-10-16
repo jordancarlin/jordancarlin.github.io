24a2af2092559985af5ea5597b8b8a32a231f831
jordancarlin <jcarlin@newroads.org>
2017-10-16 11:07:25 -0700

    create random list of words + fix letter stacking

--- hangman.js
+++ hangman.js
@@ -1,9 +1,28 @@
+var word = "";
+
+var list = ["dog",
+			"cat",
+			"dragon",
+			"cow",
+			"pig",
+			"horse",
+			"bird",
+			"griffin",
+			"tiger",
+			"lion"];
+
+function randomnumber(){
+	return Math.floor(Math.random() * 10);
+}
+
 function generateword (){
-	var word = "dog";
-	var htmlcode = "<div class='wordletters'></div>"
+	word = list[randomnumber()];
+	var htmlcode = "<div class='wordletters'></div>";
 	var wordhtml = document.getElementById("word").innerHTML
 	//i++ ----> i = i + 1
 	for(var i = 0; i < word.length; i++){
-		document.getElementById("word").innerHTML=wordhtml + htmlcode;
+		wordhtml = wordhtml+ htmlcode
+		document.getElementById("word").innerHTML=wordhtml;
 	}
 }
+
