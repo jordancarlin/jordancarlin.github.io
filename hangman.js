f1e455475bade80c866453b4fc8c4b07a91dda80
jordancarlin <jcarlin@newroads.org>
2017-10-30 11:41:45 -0700

    accept keyboard letters

--- hangman.js
+++ hangman.js
@@ -11,9 +11,32 @@ var list = ["dog",
 			"tiger",
 			"lion"];
 
+var listofletters = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '];
+
+var index = 0;
+
+function startgame(){
+
+}
+
+document.onkeypress = function(e) {
+	e = e || window.event;
+	//get letters html
+	var lettersHTML = document.getElementById("char").innerHTML;
+	//update html wiht new key pressed
+	lettersHTML = lettersHTML + " " + String.fromCharCode(e.keyCode);
+	//update html of char
+	document.getElementById("char").innerHTML = lettersHTML;
+	//check for duplicate letters
+	listofletters[index] = String.fromCharCode(e.keyCode);
+	index = index + 1;
+};
+
+
+
 function randomnumber(){
 	return Math.floor(Math.random() * 10);
-}
+}	
 
 function generateword (){
 	word = list[randomnumber()];
